e0d59ea02dc0903332c629d2b70f758321cacad3
nabilaalstr <nabilaayudyal13@gmail.com>
2023-06-12 15:55:40 +0800

    melengkapi method matematika

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -3,19 +3,29 @@ package hitung;
 
 
 public class matematika {
-
+   //membuat variabel
    private double bil1,bil2;
    
-   
+   //membuat constructor
    public matematika(double bil1, double bil2){
        this.bil1 = bil1;
        this.bil2 = bil2;
    }
-   
+   //membuat method setpenjumlahan
    double setpenjumlahan(){
-       return bil1 + bil2;
-       
-       
+       return bil1 + bil2;   
+   }
+   //membuat method setpengurangan
+   double setpengurangan(){
+       return bil1 - bil2;   
+   }
+   //membuat method setperkalian
+   double setperkalian(){
+       return bil1 * bil2;   
+   }
+   //membuat method setpembagian
+   double setpembagian(){
+       return bil1 / bil2;   
    }
     
 }
